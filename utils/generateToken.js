911fa92c2d531217231174ad090f5a9ee350a827
Tomiwa Adelae <thetommedia@gmail.com>
2024-03-24 00:04:10 +0100

    Changed the Cors settings

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,9 +7,8 @@ const generateToken = (res, userId) => {
 
 	res.cookie("jwt", token, {
 		httpOnly: true,
-		// secure: process.env.NODE_ENV !== "development",
-		secure: false,
-		sameSite: "lax",
+		secure: process.env.NODE_ENV !== "development",
+		sameSite: "none",
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 };
